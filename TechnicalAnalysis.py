c56fb46221028e85bdc11e6087e9723363c439e8
ptruong <ptruong@kth.se>
2018-07-16 17:35:16 +0200

    Updated TA script with ADL, plotting functions, OCHL etc.

--- TechnicalAnalysis.py
+++ TechnicalAnalysis.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib.finance as finplt
 
 
 
@@ -94,14 +95,92 @@ def plot_RSI(rsi_data, overbought = 70, oversold = 30):
     ax = main_data.RSI.plot()
     ax.axhline(70, color = "r")
     ax.axhline(30, color = "r")
-    
 
+def RSI_signal(rsi_data, treshold_low = 30, treshold_high = 70):
+    """
+    Input RSI value, or RSI value-series. 
+    Get the latest signal.
+    
+    ToDo: make RSI signal series output?
+    """
+    try:
+        rsi = rsi_data[-1]
+    except:
+        rsi = rsi_data
+    
+    if rsi < 0:
+        raise ValueError("Input value: "+str(rsi)+". RSI values should be between [0,100].")
+    elif rsi > 100:
+        raise ValueError("Input value: "+str(rsi)+". RSI values should be between [0,100].")
+        
+    if rsi < treshold_low:
+        signal = "Buy"
+    elif rsi > treshold_high:
+        signal = "Sell"
+    else:
+        signal = "Neutral"
+    return signal
+
+
+def MA_crossing(price_data, low = 50, high = 200):
+    """
+    Detect if low MA is higher than high MA.
+    """
+    if price_data.rolling(low).mean()[-1] == price_data.rolling(high).mean()[-1]:
+        signal = "Neutral"
+    else:
+        signal_boolean = (price_data.rolling(low).mean() > price_data.rolling(high).mean())[-1]
+        if signal_boolean == True:
+            signal = "Buy"
+        else:
+            signal = "Sell"
+            
+    return signal
+
+
+def ADL(df):
+    """
+    Accumulation Distribution
+    
+    Input:
+        - df with low, high, close and volume
+    """
+    money_flow_multiplier = ((df.close-df.low)-(df.high-df.close)) / (df.high-df.low) 
+    money_flow_volume = money_flow_multiplier * df.volume
+    ADL = money_flow_volume.cumsum()
+    return ADL
+
+
+def plot_ADL(df):
+    """
+    Plot ADL against close price curve
+    Input:
+        - df is DataFrame with open low,high, close
+    """
+    fig, ax1 = plt.subplots()
+    
+    #ax1.plot(ADL(df))
+    ax1 = ADL(df).plot()
+    ax1.set_ylabel("ADL")
+    
+    ax2 = ax1.twinx()
+    ax2.plot(df.close, color = "red")
+    ax2.set_ylabel("Price")
+    
+    fig.tight_layout()
+
+
+def plot_OCHL(df, up = "b", down = "r"):
+    fig, ax = plt.subplots()
+    finplt.candlestick2_ochl(ax, df.open, df.close, df.high, df.low, colorup =up, colordown=down)
 
 
 if __name__ == "__main__":
 
     plot_data(df.close)
     
+    # DataFrames
+    raw_df = df #We usually use closing prices for all analysis, therefore main_data = df.close
     main_data = pd.DataFrame(df.close, index = df.index)
     main_price = pd.DataFrame(df.close, index = df.index)
     main_returns = log_return(main_data)
@@ -115,6 +194,9 @@ if __name__ == "__main__":
     # RSI 
     main_data["RSI"] = RSI(main_price)
     plot_RSI(main_data.RSI)
+    
+    RSI_signal(main_data.RSI) 
+    
     
     
     plot_data(main_data)
