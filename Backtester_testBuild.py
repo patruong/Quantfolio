b55469df0388a1d8f1b98fb786630f62692f6cbf
ptruong <ptruong@kth.se>
2018-10-20 22:41:49 +0200

    updated testBuild file

--- Backtester_testBuild.py
+++ Backtester_testBuild.py
@@ -25,15 +25,62 @@ print(bt.talib.SMA.__doc__)
 
 class MyStrategy(bt.Strategy):
     params = dict(period1=20,
-                  period=50)
+                  period2=50,
+                  period3 = 200)
     def __init__(self):
-        self.sma = bt.indicators.SimpleMovingAverage(self.data, period=period=self.params.period1)
-        self.sma_2 = bt.talib.SMA(self.data, period=period=self.params.period2)
-    def next(self):
-        if self.sma > self.data.close:
+        self.sma_20 = bt.indicators.SimpleMovingAverage(self.data, period=self.params.period1)
+        self.sma_50 = bt.talib.SMA(self.data, timeperiod=self.params.period2)
+        self.sma_200 = bt.talib.SMA(self.data, timeperiod=self.params.period3)
+    #def next(self):
+    #    if self.sma > self.data.close:
             # Do something
-            pass
+    #        pass
 
-        elif self.sma < self.data.close:
+    #    elif self.sma < self.data.close:
             # Do something else
-            pass
+    #       pass
+    def next(self):
+    #    pass
+        if not self.position:
+            if self.sma_50 > self.sma_200:
+                self.buy(size=1)
+        else:
+            if self.sma_50 < self.sma_200:
+                self.sell(size=1)   
+#Variable for our starting cash
+startcash = 10000
+
+#Create an instance of cerebro
+cerebro = bt.Cerebro()
+
+#Add our strategy
+cerebro.addstrategy(MyStrategy)
+
+#Get Apple data from Yahoo Finance.
+data = bt.feeds.Quandl(
+    dataname='GS',
+    fromdate = datetime(2013,8,5),
+    todate = datetime(2018,8,5),
+    buffered= True,
+    apikey=API_KEY
+    )
+
+#Add the data to Cerebro
+cerebro.adddata(data)
+
+# Set our desired cash start
+cerebro.broker.setcash(startcash)
+
+# Run over everything
+cerebro.run()
+
+#Get final portfolio Value
+portvalue = cerebro.broker.getvalue()
+pnl = portvalue - startcash
+
+#Print out the final result
+print('Final Portfolio Value: ${}'.format(portvalue))
+print('P/L: ${}'.format(pnl))
+
+#Finally plot the end results
+cerebro.plot(style='candlestick')
