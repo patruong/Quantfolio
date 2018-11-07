845f6193d58b75b18acd6d8142b1827c86a58988
John Anderson <ptruong@kth.se>
2018-11-07 17:08:17 +0100

    added svm+featureengineering start

---
+++ SVM_featureEngineering.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 17:06:23 2018
+
+@author: ptruong
+
+Implement SVM with feature engineering, 10 technical indicators as features.
+"""
